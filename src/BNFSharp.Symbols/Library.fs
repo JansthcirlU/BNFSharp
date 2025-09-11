db0a89a8e74724627e209aee07b695a41906ff4c
Jan Ulrichts <jan.ulrichts@gmail.com>
2025-09-11 15:32:26 +0200

    Created base project hierarchy.

---
+++ src/BNFSharp.Symbols/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace BNFSharp.Symbols
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ src/BNFSharp.TypeGeneration/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace BNFSharp.TypeGeneration
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
