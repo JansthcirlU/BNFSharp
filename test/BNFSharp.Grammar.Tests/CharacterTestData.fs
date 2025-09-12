0ef4d1225a0e38ada08f2d13092aa707eb6d3513
Jan Ulrichts <jan.ulrichts@gmail.com>
2025-09-12 12:11:01 +0200

    Added some sanity check tests.

---
+++ test/BNFSharp.Grammar.Tests/CharacterTestData.fs
@@ -0,0 +1,51 @@
+module CharacterTestData
+
+let toTheoryData (s: string) =
+    s
+    |> Seq.map (fun c -> [| box c |])
+    |> Seq.toList
+
+// Valid per type
+let ValidLetters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let ValidLetterData =
+    ValidLetters
+    |> toTheoryData
+
+let ValidDigits =
+    "0123456789"
+let ValidDigitData =
+    ValidDigits
+    |> toTheoryData
+
+let ValidSymbols =
+    "|!#$%&()*+,-./:;>=<?@[\\]^_{}~"
+let ValidSymbolData =
+    ValidSymbols
+    |> toTheoryData
+
+// Invalid per type (test mutual exclusivity)
+let InvalidLetters =
+    ValidDigits + ValidSymbols
+let InvalidLetterData =
+    InvalidLetters
+    |> toTheoryData
+
+let InvalidDigits =
+    ValidLetters + ValidSymbols
+let InvalidDigitData =
+    InvalidDigits
+    |> toTheoryData
+
+let InvalidSymbols =
+    ValidLetters + ValidDigits
+let InvalidSymbolData =
+    InvalidSymbols
+    |> toTheoryData
+
+// Valid characters
+let ValidCharacters =
+    ValidLetters + ValidDigits + ValidSymbols
+let ValidCharacterData =
+    ValidCharacters
+    |> toTheoryData
